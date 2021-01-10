1e1600a4d1f9f8e547e6f6dbc81a8fb6c54842cf
Ivy Zhang <Zivy042003@gmail.com>
2021-01-10 13:46:56 -0500

    Nav fixed

--- src/framework2/BotMuckraker.java
+++ src/framework2/BotMuckraker.java
@@ -23,7 +23,9 @@ public class BotMuckraker extends Bot {
 		}
 
         while (true) {
+        	System.out.println(rc.getRoundNum());
             try {
+            	System.out.println("TAKING TURN");
                 turn();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -33,7 +35,10 @@ public class BotMuckraker extends Bot {
     }
 
     public static void turn() throws GameActionException {
+    	// Update bot location
 		here = rc.getLocation();
+
+		System.out.println("Searching for slanderers to destroy");
         int actionRadius = rc.getType().actionRadiusSquared;
         int sensorRadius = rc.getType().sensorRadiusSquared;
         // Destroy slanderer if in range.
@@ -45,37 +50,47 @@ public class BotMuckraker extends Bot {
                 }
             }
         }
+		System.out.println("No slanderers in range to destroy");
 
 		NavPolicy navPolicy = new PolicyAvoidFriendlyMuckrakers(rc.senseNearbyRobots(sensorRadius, us));
 
+
         // Find closest slanderer around to chase
+		targetDistance = 1000;
+		target = null;
 		for (RobotInfo robot : rc.senseNearbyRobots(sensorRadius, them)) {
-			targetDistance = 1000;
-			target = null;
 			if (robot.type.canBeExposed()) {
 				int distanceFrom = here.distanceSquaredTo(robot.location);
 				if (distanceFrom < targetDistance) {
 					target = robot.location;
+					targetDistance = distanceFrom;
 				}
 			}
 		}
+		System.out.println("Finished finding slanderer");
 
 		// Chase slanderer if any found.
 		if (target != null) {
+			System.out.println("Chasing slanderer");
 			Nav.goTo(target, navPolicy);
 			return;
 		}
+		System.out.println("No chasing");
 
 		// If you hit a border, choose a new random direction to scout
-		if (!rc.canSenseLocation(here.add(dir))) {
+		if (!rc.onTheMap(here.add(dir))) {
+			System.out.println("HIT BORDER");
 			int times = (int)(Math.random() * 6) + 1;
+			System.out.println("ROTATING " + times + " times");
 			for (int i = 0; i < times; ++i) 
-				dir.rotateLeft();
+				dir = dir.rotateLeft();
+			System.out.println("NEW DIRECTION: " + dir);
 		}
 
 		// TODO: Some way to report back to EC if you find an enemy / neutral EC
 		// Move in direction if no slanderers near
 		// TODO: Fix, they don't move at the very start for unknown reasons and also don't adjust direction. (bug)
+		System.out.println(dir + " " + rc.canMove(dir));
 		Nav.moveDirection(dir, navPolicy);
     }
 }
